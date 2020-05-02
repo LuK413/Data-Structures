725584cdb51ea68c0e970d7cd08480c09fbac2cd
Kai Lu <kaisiulu@gmail.com>
2020-05-02 13:24:21 -0400

    integers. fix templating error

--- Stack/main.cc
+++ Stack/main.cc
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <exception>
+#include <string>
+#include "stack.h"
 
 int main(int argc, char const *argv[]) {
-    
+    // Stack<int> s;
+    Stack s;
+    if (s.empty())
+        std::cout << "empty" << std::endl;
+    s.push(1);
+    s.push(5);
+    for (int i = 0; i < s.getSize(); ++i) {
+        std::cout << s.get(i) << std::endl;
+    }
+    s.push(3);
+    s.push(33);
+    s.push(2312);
+    std::cout << s.top() << std::endl;
+    std::cout << "just popped " << s.pop() << " from stack" << std::endl;
+    for (int i = 0; i < s.getSize(); ++i) {
+        std::cout << s.get(i) << std::endl;
+    }
     return 0;
 }
--- Stack/stack.h
+++ Stack/stack.h
@@ -1,19 +1,44 @@
 #ifndef STACK_H
 #define STACK_H
 
-template <typename T>
+#include <iostream>
+
+// template <typename T>
+// class Stack {
+// private:
+//     T *contents;
+//     int size;
+//     int capacity;
+// public:
+//     int getSize();
+//     T get(int num);
+//     Stack();
+//     void push(T data);
+//     T pop();
+//     bool empty();
+//     T top();
+//     ~Stack();
+
+//     // friend std::ostream &operator<<(std::ostream &out, const Stack<T> &s);
+// };
+
 class Stack {
 private:
-    T *contents;
+    int *contents;
     int size;
     int capacity;
 public:
+    int getSize();
+    int get(int num);
     Stack();
-    void push(T data);
-    T pop();
+    void push(int data);
+    int pop();
     bool empty();
-    T top();
+    int top();
     ~Stack();
+
+    // friend std::ostream &operator<<(std::ostream &out, const Stack<T> &s);
 };
+
 
 #endif
