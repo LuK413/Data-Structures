bca4bf1f7138f479474a53b1835668a6373f97e8
Kai Lu <kaisiulu@gmail.com>
2020-05-11 23:42:23 -0400

    corrected function signature

--- Queue/queue.py
+++ Queue/queue.py
@@ -5,15 +5,25 @@ class Queue:
         self.contents = []
         self.size = 0
 
-    def enqueue(item):  # add item
+    def enqueue(self, item):  # add item
         self.contents.append(item)
         self.size += 1
 
-    def dequeue():  # remove item
+    def dequeue(self):  # remove item
         self.contents.remove(self.contents[0])
+        self.size -= 1
 
-    def empty():
-        return size == 0
+    def empty(self):
+        return self.size == 0
 
-    def top():
+    def top(self):
         return self.contents[self.size - 1]
+
+
+queue = Queue()
+
+print(queue.empty())
+
+print(queue.enqueue(4))
+print(queue.enqueue(2))
+print(queue.top())
