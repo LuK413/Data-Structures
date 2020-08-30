745ed34356e1938189b501f0bdec74d768d10c7f
Kai Lu <kaisiulu@gmail.com>
2020-08-30 17:03:29 -0400

    finish big 5 for LL

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -1,27 +1,47 @@
-Node::Node(int data) {
-    this->data = data;
+#include "linkedlist.h"
+#include <utility>
+
+Node::Node(int data, Node *next): data{data}, next{next} {}
+
+Node::Node(const Node &other): data{other.data}, next{other.next ? new Node{*other.next} : nullptr} {}
+
+Node::Node(Node &&other): data{other.data}, next{other.next} {
+    other.next = nullptr;
 }
 
-LinkedList::LinkedList() {
-    head = nullptr;
+void Node::swap(Node &other) {
+    using std::swap;
+    swap(this->data, other.data);
+    swap(this->next, other.next);
 }
 
-LinkedList::addToFront(int data) {
-    Node *tmp = new Node(data);
+Node &Node::operator=(const Node &other) {
+    Node temp{other};
+    swap(temp);
+    return *this;
 }
 
-LinkedList::ithNode() {
+Node &Node::operator=(Node &&other) {
+    swap(other);
+    return *this;
+}
+
+void Node::addToFront(int data) {
 
 }
 
-LinkedList::getHead() {
+int Node::ithNode() {
 
 }
 
-LinkedList::getTail() {
+int Node::getHead() {
+     
+}
+
+int Node::getTail() {
 
 }
 
-LinkedList::~LinekdList() {
-
+Node::~Node() {
+    delete next;
 }
--- LinkedList/linkedlist.h
+++ LinkedList/linkedlist.h
@@ -1,17 +1,17 @@
-struct Node {
-    Node *next;
-    int data;
-    Node(int data);
-};
-
-class LinkedList {
+class Node {
 private:
-    Node *head;
+    int data;
+    Node *next;
+    void swap(Node &);
 public:
-    LinkedList();
+    Node(int data, Node *next);
+    Node(const Node &other);
+    Node(Node &&other);
+    Node &operator=(const Node &other);
+    Node &operator=(Node &&other);
     void addToFront(int data);
     int ithNode();
     int getHead();
     int getTail();
-    ~LinkedList();
-}
+    ~Node();
+};
